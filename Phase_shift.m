8b19c12a9f1ee7f0a2105f9ec6ad13d744e7a302
decrypt3723 <hushbeat3723@gmail.com>
2019-08-01 01:00:11 +0900

    Some error debugged

--- Phase_shift.m
+++ Phase_shift.m
@@ -1,9 +1,11 @@
+%% This function calibrates phases of the successive vital signals. Algorithm that this function uses is to estimate how many samples should be shifted by phase difference.
 function [Result] = Phase_shift(Next_Signal, Prev_freq, Prev_phase, Next_freq, Next_phase, FPS)
-phase = round(Prev_phase * FPS / Prev_freq - Next_phase * FPS / Next_freq);
+phase = round(Prev_phase * FPS / Prev_freq - Next_phase * FPS / Next_freq);	% phase parameter * sampling rate / frequency parameter(what we get is angular frequency) = the number of samples  
 avg_freq = (Prev_freq + Next_freq) / 2;
-if mod(phase, round(FPS/avg_freq)) == 0
-    Result = Next_Signal;
-else
-    Result = circshift(Next_Signal, phase);
-end
+remainder_sample = mod(phase, round(FPS/avg_freq));
+	if remainder_sample == 0
+	    Result = Next_Signal;
+	else
+	    Result = circshift(Next_Signal, remainder_sample);
+	end
 end
